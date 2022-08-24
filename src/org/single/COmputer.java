2eaf5d4da381ef93db1b362e4455f262ae1494dc
rameshalagar <rameshalagar92@gmail.com>
2022-08-25 01:22:22 +0530

    done

---
+++ src/org/single/COmputer.java
@@ -0,0 +1,8 @@
+package org.single;
+
+public class COmputer {
+public void commodl() {
+System.out.println("hcl");
+
+
+}}
---
+++ src/org/single/Destop.java
@@ -0,0 +1,15 @@
+package org.single;
+
+public class Destop extends COmputer
+{
+	private void dessize() {
+	System.out.println("17'");
+
+	}
+	public static void main(String[] args) {
+		Destop ds = new Destop();
+		ds.dessize();
+		ds.commodl();
+	}
+
+}
